00d060f2d9a0df143446a80fc421f391d31e4175
Dutchie <jvgool2001@gmail.com>
2019-10-07 22:12:46 +0200

    Added a list with the blocks

--- MultiplayerTetris/Client/Tetris/TetrisSetup.cs
+++ MultiplayerTetris/Client/Tetris/TetrisSetup.cs
@@ -6,20 +6,9 @@ using System.Threading.Tasks;
 
 namespace Client.Tetris
 {
-    class Tetrominoe
+    class DrawGame
     {
-        public static void Draw()
-        {
-            for (int i = 0; i < 23; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.SetCursorPosition(1 + 2 * j, i);
-
-                }
-
-            }
-        }
+        public static int linesCleared = 0, score = 0, level = 1;
         public static void DrawBorder()
         {
             for (int lengthCount = 0; lengthCount <= 22; lengthCount++)
@@ -32,8 +21,14 @@ namespace Client.Tetris
             Console.SetCursorPosition(0, 23);
             for (int widthCount = 0; widthCount <= 10; widthCount++)
             {
-                Console.Write("*-");
+                Console.Write("**");
             }
+            Console.SetCursorPosition(25, 0);
+            Console.WriteLine($"Level: {level} ");
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine($"Score: {score} ");
+            Console.SetCursorPosition(25, 2);
+            Console.WriteLine($"LinesCleared: {linesCleared}");
         }
     }
 }
