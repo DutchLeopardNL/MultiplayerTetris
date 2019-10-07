a419c9de87e644b0e84848add06147149b272c9a
Dutchie <jvgool2001@gmail.com>
2019-10-07 20:10:53 +0200

    Added simple border drawing

---
+++ MultiplayerTetris/Client/Tetris/DrawGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Tetris
+{
+    class Tetrominoe
+    {
+        public static void Draw()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.SetCursorPosition(1 + 2 * j, i);
+
+                }
+
+            }
+        }
+        public static void DrawBorder()
+        {
+            for (int lengthCount = 0; lengthCount <= 22; lengthCount++)
+            {
+                Console.SetCursorPosition(0, lengthCount);
+                Console.Write("|");
+                Console.SetCursorPosition(21, lengthCount);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(0, 23);
+            for (int widthCount = 0; widthCount <= 10; widthCount++)
+            {
+                Console.Write("*-");
+            }
+        }
+    }
+}
