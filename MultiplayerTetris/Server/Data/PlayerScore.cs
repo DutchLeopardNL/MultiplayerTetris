9404c553d247b5e0d5cb124e2fe3bce05b9a21fe
Wout <woutstevens@live.nl>
2019-10-15 22:10:41 +0200

    Added PlayerScore struct

---
+++ MultiplayerTetris/Server/Data/PlayerScore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerProject.Data
+{
+	public struct PlayerScore
+	{
+		public string HostName { get; set; }
+		public int Score { get; set; }
+
+		public PlayerScore(string hostName, int score)
+		{
+			this.HostName = hostName;
+			this.Score = score;
+		}
+
+		public void AddWin()
+		{
+			this.Score++;
+		}
+	}
+}
